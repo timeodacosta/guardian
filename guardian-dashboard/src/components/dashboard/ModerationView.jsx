95df52310bb105a169894a7330f24bfcf8aa06a1
dacostatimeo <timeo.dacosta77860@gmail.com>
2026-02-11 17:56:14 +0100

    update

--- guardian-dashboard/src/components/dashboard/ModerationView.jsx
+++ guardian-dashboard/src/components/dashboard/ModerationView.jsx
@@ -155,6 +155,7 @@ export default function ModerationView({
                                             value={warnConfig.autoKickCount}
                                             onChange={(e) => onConfigChange('autoKickCount', e.target.value)} // Utilise la nouvelle fonction
                                             min="0"
+                                            className="clean-input w-20 p-2 rounded text-sm text-center"
                                         />
                                         <span className="text-sm text-neutral-400">avertissements</span>
                                     </div>
